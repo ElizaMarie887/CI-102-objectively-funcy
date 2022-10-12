a89353e19e50e818775747c216c419f92e547521
Denise "Eliza" Cherry-Russell <108440338+ElizaMarie887@users.noreply.github.com>
2022-10-12 14:55:20 -0500

    Completed Assignment

--- main.js
+++ main.js
@@ -1,4 +1,47 @@
 // Your code here.
+function getFirstName(person){
+  return person.firstName;
+}
+
+function getLastName(person){
+  return person.lastName
+}
+
+function getFullName(person){
+  return `${person.firstName} ${person.lastName}`
+}
+//Pass by Reference!
+function setFirstName(person, string){
+  person.firstName = string
+}
+
+function setAge(person, age){
+  person.age = age
+}
+
+function giveBirthday(person){
+  if(person.age !== undefined){
+    person.age = person.age + 1
+  }else if (person.age === undefined){
+    person.age = 1
+  }
+}
+
+function marry(person1, person2){
+  if (person1.married === false && person2.married ===false){
+    person1.married = true;
+    person2.married = true;
+    person1.spouseName = person2.firstName + " " + person2.lastName;
+    person2.spouseName = person1.firstName + " " + person1.lastName;
+  }
+}
+
+function divorce(person1, person2){
+  person1.married = false;
+  person2.married = false;
+  delete person1.spouseName;
+  delete person2.spouseName;
+}
 
 
 
